932e9546907cada64f17691665a50405212c3f73
DhruvvGitHub <dhruvtanwani967@gmail.com>
2024-12-23 18:02:12 +0530

    added files

---
+++ script.js
@@ -0,0 +1,234 @@
+function lenis() {
+    const lenis = new Lenis()
+
+    lenis.on('scroll', ScrollTrigger.update)
+
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
+}
+lenis()
+
+
+const services = [
+    {
+        id: "01/",
+        name: "Commercial & Advertising",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "02/",
+        name: "Architecural",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "03/",
+        name: "Travel & Landscape",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "04/",
+        name: "Events & Weddings",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "05/",
+        name: "Foods & Beverages",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "06/",
+        name: "Underwater",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "07/",
+        name: "Portrait",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "08/",
+        name: "Fashion",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "09/",
+        name: "Product",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "10/",
+        name: "Sports",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "11/",
+        name: "Real Estate",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+    {
+        id: "12/",
+        name: "Editorial",
+        arrow: `<svg fill="#FFFFFF" height="30" width="30" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 330 330" xml:space="preserve">
+<path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255
+	s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0
+	c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"/>
+</svg>`
+    },
+];
+
+
+const servicesDiv = document.querySelector(".services-div");
+
+services.forEach((service) => {
+    const serviceDiv = `
+        <div class="service">
+            <div class="service-left">
+                <h5 class="tiny-heading">${service.id}</h5>
+                <h3 class="sub-title">${service.name}</h3>
+            </div>
+            ${service.arrow}
+        </div>
+    `;
+    servicesDiv.innerHTML += serviceDiv;
+});
+
+
+const serviceElements = document.querySelectorAll(".services-div .service");
+
+serviceElements.forEach(service => {
+    service.addEventListener("mouseenter", () => {
+        service.style.color = "#000000";
+
+        const svgElement = service.querySelector('svg path');
+        if (svgElement) {
+            svgElement.setAttribute('fill', '#000000');
+        }
+
+        const id = service.querySelector(".service-left h5");
+        if (id) {
+            id.style.color = "gray";
+        }
+
+        gsap.to(service.querySelector(".service-left"), {
+            transform: "translateX(12%)"
+        });
+
+        gsap.to(service.querySelector(".service svg"), {
+            rotate: "-40deg",
+            transform: "translateX(-200%)"
+        });
+    });
+
+    service.addEventListener("mouseleave", () => {
+        service.style.color = "#FFFFFF";
+
+        const svgElement = service.querySelector('svg path');
+        if (svgElement) {
+            svgElement.setAttribute('fill', '#FFFFFF');
+        }
+
+        const id = service.querySelector(".service-left h5");
+        if (id) {
+            id.style.color = "white";
+        }
+
+        gsap.to(service.querySelector(".service-left"), {
+            transform: "translateX(0%)"
+        });
+
+        gsap.to(service.querySelector(".service svg"), {
+            rotate: "0deg",
+            transform: "translateX(0%)"
+        });
+    });
+});
+
+
+
+
+const aboutUsAnimation = () => {
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".about-us",
+            scroller: "body",
+            start: "0% 70%" ,
+            end: "0% 10%",
+            markers: true,
+            scrub: 2
+        }
+    })
+
+    tl
+    .from(".lower-container", {
+        opacity: 0,
+        transform: "translateY(10%)"
+    },"a")
+    .to([".img-1",".img-3"], {
+        rotate: "-10deg",
+        x: "-400"
+    },"a")
+    .to([".img-2",".img-4"], {
+        rotate: "10deg",
+        x: "400"
+    },"a")
+}
+aboutUsAnimation()
